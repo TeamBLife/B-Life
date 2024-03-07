b849af44c6a04d898e6d3758b9f1b9a1cf37c6fb
sanghyuklee1998 <lsh980726@naver.com>
2024-03-07 14:08:59 +0900

    Refac#11

--- src/main/kotlin/com/blife/blife/domain/member/dto/MemberOwnerSignupRequest.kt
+++ src/main/kotlin/com/blife/blife/domain/member/dto/MemberOwnerSignupRequest.kt
@@ -7,7 +7,8 @@ data class MemberOwnerSignupRequest (
     val name: String,
     val email: String,
     @Schema(example = "비밀번호는 영어 소문자와 대문자, 숫자, 특수문자(~!@#\$%^&*)로 이루어져 있어야 합니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*]).{8,15}$")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*]).{8,15}$",
+        message = "비밀번호는 영어 소문자와 대문자, 숫자, 특수문자(~!@#\$%^&*)로 이루어져 있어야 합니다.")
     val password: String,
     val libCode: String
 )
--- src/main/kotlin/com/blife/blife/domain/member/dto/MemberSignupRequest.kt
+++ src/main/kotlin/com/blife/blife/domain/member/dto/MemberSignupRequest.kt
@@ -8,6 +8,7 @@ data class MemberSignupRequest(
     val name: String,
     val email: String,
     @Schema(example = "비밀번호는 영어 소문자와 대문자, 숫자, 특수문자(~!@#\$%^&*)로 이루어져 있어야 합니다.")
-    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*]).{8,15}$")
+    @Pattern(regexp = "^(?=.*[a-zA-Z])(?=.*[0-9])(?=.*[~!@#$%^&*]).{8,15}$",
+        message = "비밀번호는 영어 소문자와 대문자, 숫자, 특수문자(~!@#\$%^&*)로 이루어져 있어야 합니다.")
     val password: String
 )

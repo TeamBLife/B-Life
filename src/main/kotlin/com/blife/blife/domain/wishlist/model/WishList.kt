d7301b521565c49feb2a817857099c183946f871
modern1s <soonhyung92@gmail.com>
2024-03-13 10:29:29 +0900

    Feat : 대출 반납 시 찜 메일 알림 추가 구현 v2

--- src/main/kotlin/com/blife/blife/domain/wishlist/model/WishList.kt
+++ src/main/kotlin/com/blife/blife/domain/wishlist/model/WishList.kt
@@ -1,21 +1,21 @@
 package com.blife.blife.domain.wishlist.model
 
-import com.blife.blife.domain.book.model.Book
-import com.blife.blife.domain.book.model.LibBook
 import com.blife.blife.domain.member.model.Member
+import com.blife.blife.infra.postgresql.library.entity.LibBookEntity
 import jakarta.persistence.*
+
 @Entity(name = "wishList")
-class WishList (
-    @ManyToOne
-    @JoinColumn(name = "lib_book_id")
-    var libBook: LibBook,
+class WishList(
+	@ManyToOne
+	@JoinColumn(name = "lib_book_id")
+	var libBook: LibBookEntity,
 
-    @ManyToOne
-    @JoinColumn(name = "member_id")
-    var member : Member,
+	@ManyToOne
+	@JoinColumn(name = "member_id")
+	var member: Member,
 
-){
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    var id: Long? = null
+) {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	var id: Long? = null
 }

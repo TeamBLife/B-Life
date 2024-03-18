fb25c061af70223f3379ad5a10f3767ff79f9cac
modern1s <soonhyung92@gmail.com>
2024-03-18 20:21:58 +0900

    Bugifix: 대출 도서가 owner의 책이 맞는지 확인하는 로직 생성

--- src/main/kotlin/com/blife/blife/domain/member/model/Member.kt
+++ src/main/kotlin/com/blife/blife/domain/member/model/Member.kt
@@ -1,7 +1,9 @@
 package com.blife.blife.domain.member.model
 
+import com.blife.blife.domain.library.model.Library
 import com.blife.blife.domain.member.enums.MemberRole
 import com.blife.blife.domain.member.enums.MemberStatus
+import com.blife.blife.infra.postgresql.library.entity.LibraryEntity
 import jakarta.persistence.*
 
 
@@ -21,7 +23,11 @@ class Member(
 	val password: String,
 
 	@Column(name = "is_deleted")
-	var isDeleted: Boolean = false
+	var isDeleted: Boolean = false,
+
+	@OneToOne(mappedBy = "member", fetch = FetchType.LAZY, cascade = [CascadeType.ALL], orphanRemoval = true)
+	var library: LibraryEntity? = null // 선택적으로 LibraryEntity를 참조
+
 ) {
 	@Column(name = "status")
 	@Enumerated(EnumType.STRING)
@@ -30,4 +36,5 @@ class Member(
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	val id: Long? = null
+
 }
